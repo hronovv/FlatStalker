cbad2d2247ce872c43a4c79e09e13317be65ea41
hronovv <ivanonlane@gmail.com>
2026-07-23 14:50:23 +0300

    initalized simple bot logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
 
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+	godotenv.Load(".env")
+	token := os.Getenv("BOT_TOKEN")
+	fmt.Println(token)
+
+	b, err := bot.New(token, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module flat-scout
 
 go 1.26.2
+
+require (
+	github.com/go-telegram/bot v1.22.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
